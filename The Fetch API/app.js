d2cd679f05584f1e47054902dbfd183dbecf7017
Joseph Arias <arias.joseph.a@gmail.com>
2018-04-25 10:35:30 -0400

    Fetch

---
+++ The Fetch API/app.js
@@ -0,0 +1,55 @@
+
+document.getElementById('button1').addEventListener('click', getText);
+document.getElementById('button2').addEventListener('click', getJson);
+document.getElementById('button3').addEventListener('click', getExternal);
+
+
+// GET local text file data using Fetch
+function getText() {
+  fetch('test.txt')
+  .then(function(response) {
+    return response.text();
+  })
+  .then(function(data) {
+    document.getElementById('output').innerHTML = data;
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+}
+
+// GET local JSON data 
+function getJson() {
+  fetch('posts.json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    let output = '';
+    data.forEach(function(post) {
+      output += `<li> ${post.title} </li>`;
+    });
+    document.getElementById('output').innerHTML = output;
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+}
+
+// GET from External API
+function getExternal() {
+  fetch('https://api.github.com/users')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(data) {
+    let output = '';
+    data.forEach(function(user) {
+      output += `<li> ${user.login} </li>`;
+    });
+    document.getElementById('output').innerHTML = output;
+  })
+  .catch(function(err) {
+    console.log(err);
+  });
+}
