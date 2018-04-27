eab7a1ed212e8001e71d5bb17b8b354142013cb2
Joseph Arias <arias.joseph.a@gmail.com>
2018-04-27 03:20:15 -0400

    Fetch & Promises

--- Custom HTTP Library (Fetch & Promises)/easyhttp2.js
+++ Custom HTTP Library (Fetch & Promises)/easyhttp2.js
@@ -1,6 +1,6 @@
 
 class EasyHTTP {
-  // Make a GET REQUEST
+  // Make an HTTP GET REQUEST
   get(url) {
     return new Promise((resolve,reject) => {
       fetch(url)
@@ -9,4 +9,52 @@ class EasyHTTP {
       .catch(err => reject(err));
     });
   }
+
+  // Make an HTTP POST REQUEST
+  post(url,data) {
+    return new Promise((resolve,reject) => {
+      fetch(url, {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      .then(response => response.json())
+      .then(data => resolve(data))
+      .catch(err => reject(err));
+    });
+  }
+
+  // Make an HTTP PUT REQUEST
+  put(url,data) {
+    return new Promise((resolve,reject) => {
+      fetch(url, {
+        method: 'PUT',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+      })
+      .then(response => response.json())
+      .then(data => resolve(data))
+      .catch(err => reject(err));
+    });
+  }
+
+  // Make an HTTP DELETE REQUEST
+  delete(url) {
+    return new Promise((resolve,reject) => {
+      fetch(url, {
+        method: 'DELETE',
+        headers: {
+          'Content-type': 'application/json'
+        },
+      })
+      .then(response => response.json())
+      .then(() => resolve('Resource Deleted...'))
+      .catch(err => reject(err));
+    });
+  }
+
 }
